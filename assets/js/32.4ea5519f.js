1dbd33144646b35cc6a973cce4c5efd4f033fec4
sinajia <lifeign@proton.me>
2024-02-20 01:35:07 +0000

    Deploying to ph-pages from @ BeWaterXYZ/bewater-docs@cfde5d3eda4051725a537db48240d5dcb496e930 🚀

---
+++ assets/js/32.4ea5519f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{330:function(t,e,n){"use strict";n.r(e);var s={beforeMount(){window.location.href="/guide/"}},o=n(7),i=Object(o.a)(s,(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("div",{staticClass:"features"})])}),[],!1,null,null,null);e.default=i.exports}}]);
